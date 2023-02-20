54b882d8cf8a2b8c83eb94a517d357677ac27640
GameDog9988 <alex@grattan.me>
2023-02-19 21:38:52 -0500

    feat: day6

---
+++ day6/part-two.ts
@@ -0,0 +1,15 @@
+const text = await Deno.readTextFile("./input.txt");
+
+let startPointer = 0;
+let endPointer = 14;
+
+while (
+  new Set(text.slice(startPointer, endPointer)).size < 14 &&
+  endPointer <= text.length
+) {
+  startPointer++;
+  endPointer++;
+}
+
+console.log("Answer is", endPointer);
+console.log("Marker was", text.slice(startPointer, endPointer));
